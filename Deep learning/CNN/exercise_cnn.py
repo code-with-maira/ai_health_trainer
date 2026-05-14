4e8a85cfa183563e09dcd6afb0c6374ab65daa13
MAIRA SHAHZAD <mairashahzad027@gmail.com>
2026-05-14 20:53:14 +0500

    update

--- Deep learning/CNN/exercise_cnn.py
+++ Deep learning/CNN/exercise_cnn.py
@@ -61,7 +61,7 @@ train_gen = train_datagen.flow_from_directory(
 )
 
 val_gen = train_datagen.flow_from_directory(
-    'dataset/exercises_recognization/',
+    'dataset/exercise_recognization/',
     target_size=(64, 64),
     batch_size=32,
     class_mode='categorical',
